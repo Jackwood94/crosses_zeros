c7ab23cf797674b41624e25a0d0902e104f8f48b
Jackwood94 <134733312+Jackwood94@users.noreply.github.com>
2023-09-07 17:07:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -73,9 +73,9 @@ function makeComputerMove() {
 function checkWin(board, player) {
     console.log("checkWin")
     const winningCombos = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Горизонтальные
-        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Вертикальные
-        [0, 4, 8], [2, 4, 6]           // Диагональные
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+        [0, 4, 8], [2, 4, 6]           
     ];
 
     for (const combo of winningCombos) {
